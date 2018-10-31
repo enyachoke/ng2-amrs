cb5de9baf3337cf0deba8c01c5dcdb5caef5fce0
Emmanuel Nyachoke <achachiez@gmail.com>
2018-10-31 12:40:46 +0300

    WIP

---
+++ src/app/patient-dashboard/hiv/genexpert-images/genexpert-images.component.ts
@@ -0,0 +1,17 @@
+import { OnInit, OnDestroy, Component } from "@angular/core";
+
+@Component({
+    selector: 'genexpert-images',
+    templateUrl: './genexpert-images.component.html',
+    styleUrls: []
+})
+export class GeneXpertImagesComponent implements OnInit, OnDestroy {
+   
+    ngOnInit(): void {
+        
+    }
+
+    ngOnDestroy(): void {
+        
+    }
+}
